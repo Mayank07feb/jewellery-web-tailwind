0eb7b7faf6ac198d25ddd2a4dfbdecf382927e0f
Mayank Sharma <makedy07feb@gmail.com>
2024-05-31 15:49:27 +0530

    first commit

--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,37 @@
+// script.js
 
-const menuToggle = document.getElementById('menu-toggle');
-const mobileMenu = document.getElementById('mobile-menu');
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
 
-menuToggle.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
+    });
+
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slide');
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        currentIndex = (index + slides.length) % slides.length;
+        slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    prevButton.addEventListener('click', () => {
+        showSlide(currentIndex - 1);
+    });
+
+    nextButton.addEventListener('click', () => {
+        showSlide(currentIndex + 1);
+    });
+
+    setInterval(() => {
+        showSlide(currentIndex + 1);
+    }, 5000);
+
+    const menuToggle = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuToggle.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
 });
-
-
-AOS.init();
