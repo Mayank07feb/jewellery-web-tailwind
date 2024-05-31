1d4a97fa98e38996298c63c670db964e8876ce69
Mayank Sharma <makedy07feb@gmail.com>
2024-05-31 17:02:56 +0530

    first commit

--- public/scripts.js
+++ public/scripts.js
@@ -1,32 +1,9 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', () => {
     AOS.init({
 
     });
 
-    const slider = document.querySelector('.slider');
-    const slides = document.querySelectorAll('.slide');
-    const prevButton = document.getElementById('prev');
-    const nextButton = document.getElementById('next');
-    let currentIndex = 0;
-
-    function showSlide(index) {
-        currentIndex = (index + slides.length) % slides.length;
-        slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-    }
-
-    prevButton.addEventListener('click', () => {
-        showSlide(currentIndex - 1);
-    });
-
-    nextButton.addEventListener('click', () => {
-        showSlide(currentIndex + 1);
-    });
-
-    setInterval(() => {
-        showSlide(currentIndex + 1);
-    }, 5000);
+    // Removed the slider functionality
 
     const menuToggle = document.getElementById('menu-toggle');
     const mobileMenu = document.getElementById('mobile-menu');
